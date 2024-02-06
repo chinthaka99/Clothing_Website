98da9a08c5e6fe2c9bb31709e45b571a84a3c896
chinthaka99 <dilhanchinthaka99@gmail.com>
2024-02-07 02:53:15 +0530

    commit 2

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Navbar from './components/navbar/Navbar.jsx';
+import Shop from './pages/Shop.jsx';
+import ShopCategory from './pages/ShopCategory.jsx';
+import Product from './pages/Product.jsx'
 
 function App() {
   return (
     <div>
       <BrowserRouter>
         <Navbar>
+          <Routes>
+            <Route path='/' element={<Shop/>}></Route>
+            <Route path='/mens' element={<ShopCategory category = "mens"/>}></Route>
+            <Route path='/womens' element={<ShopCategory category = "womens"/>}></Route>
+            <Route path='/womens' element={<ShopCategory category = "kids"/>}></Route>
+            <Route path='/product' element={<Product/>}>
+
+            </Route>
+          </Routes>
 
         </Navbar>
       </BrowserRouter>
